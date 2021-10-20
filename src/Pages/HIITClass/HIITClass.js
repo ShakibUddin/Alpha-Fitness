35ca46fad8d45ea677b6a71b1075718cbe055288
ShakibUddin <shakibuddinbhuiyan@gmail.com>
2021-10-20 23:30:03 +0600

    updated hiit and special page ui

--- src/Pages/HIITClass/HIITClass.js
+++ src/Pages/HIITClass/HIITClass.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import useData from "../../Hooks/useData";
-import hiitZone from '../../images/hitzone.jpg';
+import hiitbg from '../../images/hiitbg.jpg';
 import HIItCard from './HIItCard/HIItCard';
 
 const HIITClass = () => {
     const { HIITClasses } = useData();
     return (
         <div className="w-full flex flex-col justify-center">
-            <img className="w-3/5 mx-auto my-20" src={hiitZone} alt="" />
+            <div className="w-full h-screen object-cover bg-fixed bg-blue-600 bg-blend-multiply relative" style={{ backgroundImage: `url(${hiitbg})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+                <p className="font-bold text-3xl text-center lg:text-7xl py-3 text-yellow-400 absolute top-40 uppercase">The benefits of HIIT include burning calories, losing weight, and building muscle.</p>
+            </div>
             <div className="flex flex-wrap justify-evenly">
                 {
                     HIITClasses.map(item => <HIItCard key={item.id} data={item}></HIItCard>)
--- src/Pages/SpecialClasses/SpecialClasses.js
+++ src/Pages/SpecialClasses/SpecialClasses.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import useData from '../../Hooks/useData';
+import specialbg from '../../images/specialbg.jpg';
 import SpecialClassCard from './SpecialClassCard/SpecialClassCard';
 
 const SpecialClasses = () => {
     const { specialClasses } = useData();
     return (
         <div className="w-full flex flex-col justify-center">
-            <p className="font-extrabold lg:text-5xl text-3xl text-center p-3 mt-16 mb-2 text-blue-500">Join our special class and persue excellence</p>
+            <div className="w-full h-screen object-cover bg-fixed bg-blue-600 bg-blend-multiply relative" style={{ backgroundImage: `url(${specialbg})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+                <p className="font-bold text-3xl text-center lg:text-7xl py-3 text-yellow-400 absolute top-40 uppercase">Join our special class and persue excellence</p>
+            </div>
             <div className="flex flex-wrap justify-evenly">
                 {
                     specialClasses.map((item, index) => <SpecialClassCard key={item.id} data={item} index={index}></SpecialClassCard>)
